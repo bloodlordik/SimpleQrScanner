bdc5364472e48a1e666fed6269c8e41a1bd4eef6
Roma Kirshiv <bloodlordik@yandex.ru>
2021-12-24 17:18:21 +0700

    Create Image Analyzer and Camera permission

--- app/src/main/java/ru/kirshov/simpleqrscanner/MainActivity.kt
+++ app/src/main/java/ru/kirshov/simpleqrscanner/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+
             SimpleQrScannerTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
