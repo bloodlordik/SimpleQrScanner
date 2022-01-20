729189cca56016f021a4d3f9a1631b6e47a4ea80
Roma Kirshiv <bloodlordik@yandex.ru>
2022-01-20 20:11:07 +0700

    Refactoring code

--- app/src/main/java/ru/kirshov/simpleqrscanner/ui/ResultWidget.kt
+++ app/src/main/java/ru/kirshov/simpleqrscanner/ui/ResultWidget.kt
@@ -1,13 +1,16 @@
 package ru.kirshov.simpleqrscanner.ui
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import ru.kirshov.simpleqrscanner.tools.rememberToast
 
@@ -19,8 +22,15 @@ fun ResultWidget(result:String){
     Box(modifier = Modifier
         .fillMaxWidth()
         .padding(24.dp)
-        .clickable{ toast.show()}, contentAlignment = Alignment.Center){
+        .clickable { toast.show() }
+        .background(color = MaterialTheme.colors.secondary), contentAlignment = Alignment.Center){
         Text(text = result)
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ResultWidgetPreview(){
+    ResultWidget(result = "ggggggggdgdggdd")
 }
 
--- app/src/main/java/ru/kirshov/simpleqrscanner/ui/Screen.kt
+++ app/src/main/java/ru/kirshov/simpleqrscanner/ui/Screen.kt
@@ -1,18 +1,8 @@
 package ru.kirshov.simpleqrscanner.ui
 
-import android.Manifest
-import android.content.pm.PackageManager
-import androidx.activity.compose.rememberLauncherForActivityResult
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.camera.core.CameraSelector
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.unit.dp
-import androidx.core.content.ContextCompat
 import ru.kirshov.simpleqrscanner.R
 import ru.kirshov.simpleqrscanner.tools.RequestPermissionDialog
 
